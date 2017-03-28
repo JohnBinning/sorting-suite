3d6f5ede296632a0e1518a06dc1cb897ac17b027
John Binning <john.binning17@gmail.com>
2017-03-28 17:03:28 -0600

    refactor bubble for es lint

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -17,8 +17,8 @@ let container;
 
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
-      for (var j = 0; j < arr.length - 1; j++) {
-        if (arr[j + 1] < arr[j]) {
+    for (var j = 0; j < arr.length - 1; j++) {
+      if (arr[j + 1] < arr[j]) {
         container = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = container
