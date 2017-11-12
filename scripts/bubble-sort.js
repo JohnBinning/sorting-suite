eccb0e375f893c4b0f9fae20c78a0650e4b5235b
John Binning <john.binning17@gmail.com>
2017-11-12 13:17:38 -0700

    spacing on bubble-sort

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -12,7 +12,8 @@
 //   return arr;
 // }
 
-// non-recursive - faster
+// non-recursive = faster
+
 export const bubbleSort = arr => {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
